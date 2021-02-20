4ff9f3fd6011a58c708b04bd7423c40cf1321726
gaoguanqi <307590625@qq.com>
2021-02-20 20:50:30 +0800

    goods

---
+++ src/components/goods/Goods.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>Goods</div>
+</template>
+<script>
+export default {
+  name: 'goods',
+}
+</script>
+<style lang="less" scoped>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,8 @@ import Rights from '../components/power/Rights.vue'
 import Roles from '../components/power/Roles.vue'
 import Cate from '../components/goods/Cate.vue'
 import Params from '../components/goods/Params.vue'
+import Goods from '../components/goods/Goods.vue'
+
 
 Vue.use(VueRouter)
 
@@ -24,6 +26,7 @@ const routes = [
       { path: '/roles', component: Roles },
       { path: '/categories', component: Cate },
       { path: '/params', component: Params },
+      { path: '/goods', component: Goods },
     ]
   }
 ]
